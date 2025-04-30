d72910395466e7ca748982fc7d161f42419376bf
Facundo Contreras <facundocontre4375@gmail.com>
2025-04-30 19:20:45 -0300

    Revert "Creo clases (Area y grupo) y sus test, tarea #29, #30, #31 y #32"

--- src/validators/AreaValidator.ts
+++
@@ -1,9 +0,0 @@
-import { BaseValidator } from "./BaseValidator";
-
-export class AreaValidator extends BaseValidator {
-    static validate(nombre: string): void {
-        this.validateRequired(nombre, "nombre")
-
-        this.validateString(nombre, "nombre")
-    }
-}
