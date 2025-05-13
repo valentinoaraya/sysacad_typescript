857ce1c5a3eef8f15433922bff591213b88464d5
valentinoaraya <valentinoaraya04@gmail.com>
2025-05-13 13:37:24 -0300

    Implemento test actualizarUniversidad

--- src/repositories/UniversidadRepository.ts
+++ src/repositories/UniversidadRepository.ts
@@ -18,16 +18,34 @@ export class UniversidadRepository {
             throw new Error(`Error al crear Universidad: ${error}}`)
         }
     }
+
     static async buscarPorId(id: number): Promise<UniversidadAtributos | null> {
         try {
 
             const universidadBD = await prisma.universidades.findUnique({ where: { id } });
-            if (!universidadBD) throw new Error(`No se encontró Universidad con el id: ${id}`)
+            if (!universidadBD) throw new Error(`No se encontró Universidad con el id ${id}`)
 
             return universidadBD
 
         } catch (error: any) {
-            throw new Error(`Error al obtener una Universidad con el id: ${id}}`)
+            throw new Error(`Error al obtener una Universidad con el id ${id}}`)
+        }
+    }
+
+    static async actualizar(id: number, nuevosDatos: Partial<UniversidadAtributos>): Promise<UniversidadAtributos> {
+        try {
+
+            const universidadActualizada = await prisma.universidades.update({
+                where: { id },
+                data: nuevosDatos
+            })
+
+            if (!universidadActualizada) throw new Error(`No se encontró universidad con el id ${id}}`)
+
+            return universidadActualizada
+
+        } catch (error: any) {
+            throw new Error(`Error al actualizar datos de la universidad: ${error}}`)
         }
     }
 
--- src/services/UniversidadService.ts
+++ src/services/UniversidadService.ts
@@ -2,11 +2,17 @@ import { UniversidadRepository } from "../repositories/UniversidadRepository";
 import { UniversidadAtributos } from "../types";
 
 export class UniversidadService {
+
     static crearUniversidad(universidad: UniversidadAtributos): Promise<UniversidadAtributos> {
         return UniversidadRepository.crear(universidad);
     }
+
     static obtenerUniversidadPorId(id: number): Promise<UniversidadAtributos | null> {
         return UniversidadRepository.buscarPorId(id);
+    }
+
+    static actualizarUniversidad(id: number, nuevosDatos: Partial<UniversidadAtributos>): Promise<UniversidadAtributos> {
+        return UniversidadRepository.actualizar(id, nuevosDatos)
     }
 
 }
