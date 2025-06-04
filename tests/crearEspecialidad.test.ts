6cc8eb4945e88b3d0db3005041c37d2f2e81ee8d
valentinoaraya <valentinoaraya04@gmail.com>
2025-06-04 17:09:26 -0300

    Creo script para limpiar base de datos

--- tests/crearEspecialidad.test.ts
+++ tests/crearEspecialidad.test.ts
@@ -1,11 +1,11 @@
 import { EspecialidadService } from "../src/services/EspecialidadService"
 import { crearEspecialidadEjemeplo } from "../src/utils";
 
-test("Deberia crear y guardad una especialidad en la base de datos", async () =>{
+test("Deberia crear y guardad una especialidad en la base de datos", async () => {
     const especialidad = await crearEspecialidadEjemeplo();
     const especialidadCreada = await EspecialidadService.crearEspecialidad(especialidad);
     const especialidadBD = await globalThis.prisma.especialidades.findUnique({
-        where : {id : especialidadCreada.id},
+        where: { id: especialidadCreada.id },
     });
 
     expect(especialidadBD).toBeTruthy();
